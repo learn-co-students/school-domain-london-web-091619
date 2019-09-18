cd936e7422c8490f3844579d672eac3f1b0bb961
github username <github email address>
2019-09-18 16:32:26 +0100

    built the school class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require "pry"
+
+
+class School
+    attr_accessor :roster
+
+    def initialize(school_name)
+        @school = school_name
+        @roster = Hash.new {|h, k| h[k] = []}
+    end
+
+    def add_student(student_name, grade)
+        @roster[grade] << student_name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each_value { |students|
+        students.sort!
+        }
+    end
+
+end
