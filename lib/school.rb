ecc5a7918dcf0fc33618ad8993c8cf04106339c4
Luke Prendergast <luke.prendergast94@gmail.com>
2019-09-18 13:07:24 +0100

    Adding my version

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+attr_accessor :roster, :school_name, :student_name, :add_student, :grade
+
+def initialize(school_name)
+  @school_name = school_name
+  @roster = {}
+end
+
+def add_student(student_name, grade)
+  if @roster[grade]
+    @roster[grade] << student_name
+  else
+    @roster[grade] = []
+    @roster[grade] << student_name
+  end
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each_value{|v| v.sort!}
+end
+end
